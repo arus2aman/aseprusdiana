1dae671ec419b4cd7ec196d3a719f1613bff2228
arus2aman <chikaadisini@gmail.com>
2024-06-28 11:05:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st 
+import pandas as pd 
+import requests
+from st_aggrid import AgGrid
 
 def utama() : 
   st.write('Minimal Example')
@@ -13,5 +16,26 @@ def main() :
   st.write('Some Phytagorean Equation : ')
   st.latex('c^2 = a^2+b^2')
 
+if __name__ == '__main__' : 
+  main()
+
+#baca dataframe dari file csv 
+house = pd.read_csv('house_clean.csv')
+#read json file dari data covid 
+flight_passanger_api = requests.post('https://forecastpassengerapi.herokuapp.com/forecast_timeseries',json={
+  "month_limit": "2020-01-01",
+  "window_size": 12
+}).json()
+
+def main() : 
+  st.write('Contoh dataframe')
+  st.dataframe(house)
+  st.write('Contoh JSON')
+  st.json(flight_passanger_api)
+  st.write('Metrics')
+  st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
+  st.write('Menampilkan Dataframe dengan St AgGrid')
+  AgGrid(titanic)
+  st.table([x for x in range(1,5)])
 if __name__ == '__main__' : 
   main()
