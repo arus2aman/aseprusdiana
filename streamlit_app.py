23a8b4807ba1fade0c8a9fdd3e66cb72f8acb719
arus2aman <chikaadisini@gmail.com>
2024-06-28 10:53:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ if __name__ == '__main__' :
   utama()
 
 def main() : 
-  st.header('This is Header')
+  st.header('Halaman Streamlit Asep Rusdiana')
   st.subheader('This is SubHeader')
   st.markdown('# Rendering Markdown ')
   st.write('Some Phytagorean Equation : ')
