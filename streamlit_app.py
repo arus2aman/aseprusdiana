622b9a9a1396f1d75fa7ff3efaea139d187ff5d6
arus2aman <chikaadisini@gmail.com>
2024-06-28 13:30:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ def datahouse() :
   if check_btn :
      st.write('Anda Setuju')
   radio_button= st.radio('Choose below',[x for x in range(1,3)])
-    st.write('Anda Memilih',radio_button)
+  st.write('Anda Memilih',radio_button)
     
 if __name__ == '__main__' : 
   datahouse()
