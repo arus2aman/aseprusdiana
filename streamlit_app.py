03a73a34ed2f95e4fa7f73547301f0110e846675
arus2aman <chikaadisini@gmail.com>
2024-06-28 14:11:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,6 +87,10 @@ def datahouse() :
   tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
   tab1.write("this is tab 1")
   tab2.write("this is tab 2")
+  with tab1:
+    st.radio("Select one:", [1, 2])
+  with tab2:
+     st.button('Click Aing')
 
 if __name__ == '__main__' : 
   datahouse()
