6b534b14b906f32fa8e15ab8ad0bdbac709c9b4d
arus2aman <chikaadisini@gmail.com>
2024-06-28 13:29:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,22 @@ house = pd.read_csv('house_clean.csv')
 def datahouse() : 
   st.write('Contoh dataframe')
   st.dataframe(house)
+  ##Untuk membuat metrik
   st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
   st.write('Menampilkan Dataframe dengan St AgGrid')
+
+  ##untuk menampilkan Aggrid--masih error
   #AgGrid(house)
   #st.table([x for x in range(1,5)])
-  
+  ###untuk membuat tombol
+  click_me_btn = st.button('Click Me')
+  st.write(click_me_btn) #Return True kalo di Click 
+  check_btn = st.checkbox('Klik Jika Setuju')
+  if check_btn :
+     st.write('Anda Setuju')
+  radio_button= st.radio('Choose below',[x for x in range(1,3)])
+    st.write('Anda Memilih',radio_button)
+    
 if __name__ == '__main__' : 
   datahouse()
+
